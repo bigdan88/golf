b9d9394994591ddbc48e140d5e3df40a3d0e58e5
bigdan88 <daniel88han@gmail.com>
2023-02-22 22:25:06 -0500

    added the youtube stuff

--- src/App.js
+++ src/App.js
@@ -1,42 +1,58 @@
 import { useState } from "react";
-import { ColorModeContext } from "./theme";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { useMode } from "./theme";
+import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
-import { Routes, Route } from "react-router-dom";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
-// import Leaderboard from "./components/scenes/leaderboard";
-// import Players from "./components/scenes/players";
-// import Schedule from "./components/scenes/schedule";
-// import Rules from "./components/scenes/rules";
-// import ContactUs from "./components/scenes/contactus";
-// import TeamManager from "./components/scenes/teammanager";
-// import PlayerManager from "./components/scenes/playermanager";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+// import Calendar from "./scenes/calendar/calendar";
+
+import { withAuthenticator, Button, Heading, Text, TextField, View } from '@aws-amplify/ui-react';
+import { Amplify, API, graphqlOperation } from 'aws-amplify'
+import { Auth } from 'aws-amplify';
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 function App() {
-    const [theme, colorMode] = useMode();
-    const [isSidebar, setIsSidebar] = useState(true);
-  
-    return (
-      <ColorModeContext.Provider value={colorMode}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <div className="app">
-            <Sidebar isSidebar={isSidebar} />
-            <main className="content">
-              <Topbar setIsSidebar={setIsSidebar} />
-                <Routes>
-                    <Route path="/" element={<Dashboard />} />
-                    <Route path="/team" element={<Team />} />
-                </Routes>
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
-            </main>
-          </div>
-        </ThemeProvider>
-      </ColorModeContext.Provider>
-    );
-  }
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
 
-export default App;
+export default withAuthenticator(App);
